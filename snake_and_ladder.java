99947e562a44015555c1e44707a9dab5df5277a5
Sai Krishna <reachkrishnadontaraju@gmail.com>
2021-03-23 14:58:16 +0530

    generated a dice value

--- snake_and_ladder.java
+++ snake_and_ladder.java
@@ -4,6 +4,14 @@ public class snake_and_ladder {
 
 		int position = 0;
 
+		String[] storer={"One" ,"Two", "Three" ,"Four","Five","Six"};
+        	Random dice = new Random();
+
+		int displayer = dice.nextInt(6);
+        	System.out.println(storer[displayer]);
+
+
+
 	}
 
 }
