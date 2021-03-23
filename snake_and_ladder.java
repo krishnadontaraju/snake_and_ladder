bb4eb505ff922a835255fe2cf568a858f6e9df0e
Sai Krishna <reachkrishnadontaraju@gmail.com>
2021-03-23 15:11:19 +0530

    added the play positions

--- snake_and_ladder.java
+++ snake_and_ladder.java
@@ -1,3 +1,7 @@
+
+
+import java.util.Random;
+
 public class snake_and_ladder {
 
 	public static void main(String args[]) {
@@ -9,6 +13,33 @@ public class snake_and_ladder {
 
 		int displayer = dice.nextInt(6);
         	System.out.println(storer[displayer]);
+
+
+		int ladder_or_snake = dice.nextInt(3);    
+            	switch (ladder_or_snake) {
+                	case 0:
+                    		System.out.println("No play");
+                    		System.out.println(position);
+                    		                
+                    	break;
+        
+                	case 1:
+                    		System.out.println("Ladder");
+                    		position += roll;
+                        	System.out.println(position);
+                       	break;
+                    		                    		    
+                	case 2:
+                    		System.out.println("Snake");
+                    		position = position - roll;
+                    		System.out.println(position);
+                    	break;
+
+                	default :
+                    	break;
+
+
+
 
 
 
